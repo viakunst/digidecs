99c0df1fe8a5a767ccc332aceb942405b3a08742
Maarten van den Berg <maartenberg1@gmail.com>
2017-05-15 16:57:25 +0200

    Button text

--- templates/form.php
+++ templates/form.php
@@ -62,6 +62,6 @@
 			</label>
 		</div>
 		<p>Velden met een <sup>*</sup> zijn verplicht</p>
-		<input type="submit" class="btn btn-default">
+		<input type="submit" class="btn btn-success" value="Ik wil mijn geld terug!">
 	</form>
 </div>
